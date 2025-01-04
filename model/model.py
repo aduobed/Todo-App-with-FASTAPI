816f5b2706137e28c2c32f0ae66b033f5f28e936
aduobed <obedus10@gmail.com>
2025-01-04 05:01:27 -0800

    add support for user post request object model

--- model/model.py
+++ model/model.py
@@ -8,3 +8,11 @@ class TodoModel(BaseModel):
     priority: int = Field(
         gt=0, lt=6, description="Priority should be from 1 to 5")
     complete: bool = False
+
+
+class UserModel(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str 
+    password: str
