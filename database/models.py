54ab45d842f042508fd70e311a2db5a2d0d3ddd8
aduobed <obedus10@gmail.com>
2025-01-04 05:01:00 -0800

    add support for db user model

--- database/models.py
+++ database/models.py
@@ -1,5 +1,20 @@
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from database.database import Base
+from sqlalchemy.orm import relationship
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
 
 
 class Todos(Base):
@@ -10,3 +25,6 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
