8ae753b108072ae1547cf36e9160ad592fafd0e8
aduobed <obedus10@gmail.com>
2025-01-04 05:19:55 -0800

    clean todo api

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from database import models
-from database.database import engine, SessionLocal
+from database import db_start
 from sqlalchemy.orm import Session
 from exceptions.exception_handler import NoTodoFoundException, no_todo_found_exception_handler
 from model.model import TodoModel as TodoMod
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
-
 app.add_exception_handler(NoTodoFoundException,
                           no_todo_found_exception_handler)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/todo")
-async def get_todos(db: Session = Depends(get_db)):
+async def get_todos(db: Session = Depends(db_start.get_db)):
     return db.query(models.Todos).all()
 
 
 @app.get("/todo/{todo_id}")
-async def get_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
+async def get_todo_by_id(todo_id: int, db: Session = Depends(db_start.get_db)):
     query_response = db.query(models.Todos).filter(
         models.Todos.id == todo_id).first()
 
@@ -39,7 +29,7 @@ async def get_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/todo")
-async def create_todo(todo: TodoMod, db: Session = Depends(get_db)):
+async def create_todo(todo: TodoMod, db: Session = Depends(db_start.get_db)):
     db_todo = models.Todos(title=todo.title, description=todo.description,
                            priority=todo.priority, complete=todo.complete)
 
@@ -49,7 +39,7 @@ async def create_todo(todo: TodoMod, db: Session = Depends(get_db)):
 
 
 @app.put("/todo/{todo_id}")
-async def update_todo(todo_id: int, todo: TodoMod, db: Session = Depends(get_db)):
+async def update_todo(todo_id: int, todo: TodoMod, db: Session = Depends(db_start.get_db)):
     db_todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
     if db_todo is not None:
@@ -66,7 +56,7 @@ async def update_todo(todo_id: int, todo: TodoMod, db: Session = Depends(get_db)
 
 
 @app.delete("/todo/{todo_id}")
-async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+async def delete_todo(todo_id: int, db: Session = Depends(db_start.get_db)):
     db_todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
     if db_todo is None:
