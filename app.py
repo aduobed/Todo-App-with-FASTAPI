426383e61c736c012d1c5a04c0fb4137ff40b24d
aduobed <obedus10@gmail.com>
2025-01-03 17:18:51 -0800

    add post functionality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import models
 from database.database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from exceptions.exception_handler import NoTodoFoundException, no_todo_found_exception_handler
+from model.model import TodoModel as TodoMod
 
 app = FastAPI()
 
@@ -20,12 +21,12 @@ def get_db():
         db.close()
 
 
-@app.get("/todos")
+@app.get("/todo")
 async def get_todos(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
 
-@app.get("/todos/{todo_id}")
+@app.get("/todo/{todo_id}")
 async def get_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
     query_response = db.query(models.Todos).filter(
         models.Todos.id == todo_id).first()
@@ -35,3 +36,13 @@ async def get_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
     else:
         print(f"Todo with id {todo_id} not found")
         raise NoTodoFoundException(todo_id=todo_id)
+
+
+@app.post("/todo")
+async def create_todo(todo: TodoMod, db: Session = Depends(get_db)):
+    db_todo = models.Todos(title=todo.title, description=todo.description,
+                           priority=todo.priority, complete=todo.complete)
+
+    db.add(db_todo)
+    db.commit()
+    return {"message": "Todo created successfully"}
