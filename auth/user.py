343ffacb0ef6479339d68487d0977e21944137b3
aduobed <obedus10@gmail.com>
2025-01-04 18:27:03 -0800

    store user post to db

--- auth/user.py
+++ auth/user.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from database import models
+from database import db_start
 from model.model import UserModel as UserMod
+from sqlalchemy.orm import Session
 from utils.password_hash import get_password_hash
 
 app = FastAPI()
 
 
 @app.post("/todo/user")
-async def create_user(user: UserMod):
+async def create_user(user: UserMod, db: Session = Depends(db_start.get_db)):
     hash_password = get_password_hash(user.password)
     user_todo = models.Users(username=user.username, email=user.email,
                              first_name=user.first_name, last_name=user.last_name, hashed_password=hash_password)
 
-    return user_todo
+    db.add(user_todo)
+    db.commit()
+    return {"message": "User created successfully"}
