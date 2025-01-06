eee02eaea866037d54d709bd267060702b221a2f
aduobed <obedus10@gmail.com>
2025-01-05 22:44:26 -0800

    add support for user post request owner id

--- model/model.py
+++ model/model.py
@@ -8,6 +8,7 @@ class TodoModel(BaseModel):
     priority: int = Field(
         gt=0, lt=6, description="Priority should be from 1 to 5")
     complete: bool = False
+    owner_id: int
 
 
 class UserModel(BaseModel):
