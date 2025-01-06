3be8984e5ad69f3f3873e9aa5910e8966cda96b4
aduobed <obedus10@gmail.com>
2025-01-05 19:57:41 -0800

    add user auth logic

---
+++ auth/user_authenticate.py
@@ -0,0 +1,12 @@
+from database import models
+from sqlalchemy.orm import Session
+from utils.password_hash import verify_password
+
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+    
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
--- utils/password_hash.py
+++ utils/password_hash.py
@@ -1,7 +1,11 @@
 from passlib.context import CryptContext
 
 
+def get_bcrypt() -> CryptContext:
+    return CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 def get_password_hash(password: str) -> str:
-    bcrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    return bcrypt.hash(password)
+    return get_bcrypt().hash(password)
  
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return get_bcrypt().verify(plain_password, hashed_password)
