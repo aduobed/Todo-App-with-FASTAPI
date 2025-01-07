8b4f0b7af36947efee22749ca997dfcfc80a9c37
aduobed <obedus10@gmail.com>
2025-01-07 15:02:11 -0800

    add postgresql connection string

--- database/database.py
+++ database/database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///todos.db" # Connection string for SQLite database
+
+
+# engine = create_engine(
+#   POSTGRES_DATABASE_URL, connect_args={"check_same_thread": False} # SQLite engine
+# )
+
+POSTGRES_DATABASE_URL = "postgresql://dark-lord:database@localhost/TodoAppDB" # Connection string for PostgreSQL database
 
 engine = create_engine(
-  SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+  POSTGRES_DATABASE_URL # PostgreSQL engine
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
