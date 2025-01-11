ec2fc18b1ef7be215124bb2686e3dd9e74c3c813
aduobed <obedus10@gmail.com>
2025-01-10 17:57:30 -0800

    added the address router

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends
 from exceptions.exception_handler import NoTodoFoundException, no_todo_found_exception_handler
-from routers import user, todos
+from routers import user, todos, address
 from utils import check_header_token
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(todos.router)
+app.include_router(address.router)
 
 app.add_exception_handler(NoTodoFoundException,
                           no_todo_found_exception_handler)
